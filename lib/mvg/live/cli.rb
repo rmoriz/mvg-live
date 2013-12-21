ac40b5d093224b07769bf130297bbe97a6a3729a
Sebastian Wilzbach <sebi@wilzbach.me>
2013-12-21 19:50:33 +0100

    added colorized cli output

--- lib/mvg/live/cli.rb
+++ lib/mvg/live/cli.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'colorize'
 
 module MVG
   class Live
@@ -52,15 +53,63 @@ module MVG
           end
 
         else
-          txt += "#{@station}: #{@transports.map(&:to_s).map(&:capitalize).join(", ")}\n"
+          strTransport = "#{@station}: #{@transports.map(&:to_s).map(&:capitalize).join(", ")}\n"
+          if @color
+            txt += strTransport.colorize(:blue)
+          else
+            txt += strTransport
+          end
           txt += "=" * 38 + "[ #{@server_time} ]=\n"
 
           @result_sorted.each do |e|
-            txt += sprintf "%-4s| %-30s|%3d Minuten\n", e[:line], e[:destination], e[:minutes]
+            if @color
+              txt += get_color_for_line((sprintf "%-4s", e[:line]), e[:line])
+              txt += sprintf "| %-30s|",e[:destination]
+              txt +=  (sprintf "%3d", e[:minutes]).colorize(:blue)
+              txt += " Minuten\n"
+            else
+              txt += sprintf "%-4s| %-30s|%3d Minuten\n", e[:line], e[:destination], e[:minutes]
+            end
           end
         end
 
         txt
+      end
+
+      def get_color_for_line(strStation, line)
+          case line
+            when "U1"
+              strStation.colorize(:green)
+            when "U2"
+              strStation.colorize(:red)
+            when "U3"
+              strStation.colorize(:yellow)
+            when "U4"
+              strStation.colorize(:cyan)
+            when "U5"
+              strStation.colorize(:blue)
+            when "U6"
+              strStation.colorize(:blue)
+            when "U6"
+              strStation.colorize(:blue)
+            when "S1"
+              strStation.colorize(:blue)
+            when "S2"
+              strStation.colorize(:green)
+            when "S3"
+              strStation.colorize(:magenta)
+            when "S4"
+              strStation.colorize(:red)
+              strStation.colorize(:blue)
+            when "S6"
+              strStation.colorize(:cyan)
+            when "S7"
+              strStation.colorize(:light_red)
+            when "S8"
+              strStation.colorize(:yellow)
+            else
+              strStation
+          end
       end
 
       def to_json
@@ -95,6 +144,11 @@ module MVG
               @station = data['default_station']
             end
 
+            if data['color']
+              @color = data['color']
+            else
+              @color = false
+            end
             @transports = data['default_transports'].map(&:downcase).map(&:to_sym) if data['default_transports']
             @using_config_file = file
             break
