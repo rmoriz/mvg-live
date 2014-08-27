28923c0d4c5b4c41d6b4da2c34d332342fc44905
Marco Eggersmann <marco.eggersmann@hotmail.de>
2014-08-27 15:28:26 +0200

    refactored convenience-hack test

--- spec/mvg_live_spec.rb
+++ spec/mvg_live_spec.rb
@@ -137,7 +137,7 @@ describe MVG::Live do
 
   describe "CLI" do
     it "should apply convenience hacks on station names" do
-      MVG::Live.any_instance.expects(:cli_station_hacks_for).with('Stachus').returns('Karlsplatz (Stachus)')
+      MVG::Live.fetch_to_display('Stachus', { :load_user_defaults => true, :cli => true }).include?("Station unknown!") .must_equal false
       mvglive = MVG::Live.fetch_to_display 'Stachus', { :load_user_defaults => true, :cli => true }
     end
 
