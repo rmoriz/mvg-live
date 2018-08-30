e772c0c73648b370ab9d69aba689d8f106f1d1ca
Michael Moll <kvedulv@kvedulv.de>
2018-08-30 18:13:35 +0200

    use dynamic attributes for factory bot

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,22 +3,22 @@ FactoryBot.define do
   end
 
   factory :westfriedhof, class: MVG::Live do
-    station 'Westfriedhof'
-    transports %i[u tram bus]
+    station { 'Westfriedhof' }
+    transports { %i[u tram bus] }
   end
 
   factory :hackerbruecke, class: MVG::Live do
-    station 'Hackerbrücke'
-    transports %i[tram s]
+    station { 'Hackerbrücke' }
+    transports { %i[tram s] }
   end
 
   factory :goethe_institut, class: MVG::Live do
-    station 'Goethe-Institut'
-    transports [:tram]
+    station { 'Goethe-Institut' }
+    transports { [:tram] }
   end
 
   factory :leonrodplatz, class: MVG::Live do
-    station 'Leonrodplatz'
-    transports %i[tram bus]
+    station { 'Leonrodplatz' }
+    transports { %i[tram bus] }
   end
 end
